7faa53f6af666b96959f4fcd4f55af0cfa75f71c
Gera-ragwylew <113134621+Gera-ragwylew@users.noreply.github.com>
2022-09-29 18:11:00 +0300

    Update ConsoleApplication7.cpp

--- ConsoleApplication7/ConsoleApplication7.cpp
+++ ConsoleApplication7/ConsoleApplication7.cpp
@@ -1,8 +1,9 @@
-﻿#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <conio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <windows.h>
 #define board_length 80
 #define board_width 25
 #define HALF_B_W 12
@@ -10,7 +11,7 @@
 
 int Menu(int start);
 void board();
-void Move(int* move_rocket_l, int* move_rocket_r);
+void Move(int* move_rocket_l, int* move_rocket_r, int* exitp);
 void Ball(int* move_rocket_l, int* move_rocket_r, int* Ball_Y_P, int* Ball_X_P, int* flag_P, int* vector_P, int* L_P_point_P, int* R_P_point_P);
 
 int main() {
@@ -45,6 +46,8 @@ void board() {
 	int vector = rand() % 2;
 	int L_P_point = 0;
 	int R_P_point = 0;
+	int exit = 0;
+	int* exitp = &exit;
 	int* L_P_point_P = &L_P_point;
 	int* R_P_point_P = &R_P_point;
 	int* move_rocket_l = &rocketl;
@@ -54,9 +57,9 @@ void board() {
 	int* flag_P = &flag;
 	int* vector_P = &vector;
 	char board[board_width][board_length];
-	while (L_P_point != 10 && R_P_point != 10) {
+	while ((L_P_point != 10 && R_P_point != 10) && exit != 1) {
 		system("cls");
-		printf("LR%d RR%d %d %d\n", rocketl,rocketr, Ball_X, Ball_Y);
+		printf("LR%d RR%d BX%d BY%d %d\n", rocketl, rocketr, Ball_X, Ball_Y, exit);
 		printf("\t PLAYER ONE- %d", L_P_point);
 		printf("\t\t\t\t\t PLAYER TWO- %d\n", R_P_point);
 		for (int i = 0; i < board_width; i++) {
@@ -99,34 +102,49 @@ void board() {
 				}
 			}
 		}
-		Move(move_rocket_l, move_rocket_r);
+		Move(move_rocket_l, move_rocket_r, exitp);
 		Ball(move_rocket_l, move_rocket_r, Ball_Y_P, Ball_X_P, flag_P, vector_P, L_P_point_P, R_P_point_P);
+		Sleep(50);
 	}
 	system("cls");
 	if (L_P_point == 10) {
 		printf("\n\n%50cPLAYER ONE WIN!", ' ');
-	} else {
+	} else if (R_P_point == 10) {
 		printf("\n\n%50cPLAYER TWO WIN!", ' ');
+	} else {
+		printf("\n\n%50cSEE YOU SOON", ' ');
 	}
 	getchar();
 }
 
-void Move(int* move_rocket_l, int* move_rocket_r) {
-	char c;
-	scanf("%c", &c);
-	if (c == 'a' && *move_rocket_l > -10) {
-		*move_rocket_l -= 1;
-	} else if (c == 'z' && *move_rocket_l < 10) {
-		*move_rocket_l += 1;
-	} else if (c == 'k' && *move_rocket_r > -10) {
-		*move_rocket_r -= 1;
-	} else if (c == 'm' && *move_rocket_r < 10) {
-		*move_rocket_r += 1;
-	} else if (c == ' ') {
-		*move_rocket_l += 0;
-		*move_rocket_r += 0;
+void Move(int* move_rocket_l, int* move_rocket_r, int* exitp) {
+	if (_kbhit()) {
+		switch (_getch()) {
+		case 'a':
+			if (*move_rocket_l > -10) {
+				*move_rocket_l -= 1;
+				break;
+			} else break;
+		case 'z':
+			if (*move_rocket_l < 10) {
+				*move_rocket_l += 1;
+				break;
+			} else break;
+		case 'k':
+			if (*move_rocket_r > -10) {
+				*move_rocket_r -= 1;
+				break;
+			} else break;
+		case 'm':
+			if (*move_rocket_r < 10) {
+				*move_rocket_r += 1;
+				break;
+			} else break;
+		case 27:
+			*exitp = 1;
+			break;
+		}
 	}
-	fseek(stdin, 0, SEEK_END);
 }
 
 void Ball(int* move_rocket_l, int* move_rocket_r, int* Ball_Y_P, int* Ball_X_P, int* flag_P, int* vector_P, int* L_P_point_P, int* R_P_point_P) {
